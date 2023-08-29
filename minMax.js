3f04ba8aad1562573ee8d87a2f73b269d543715b
channaveer <channaveerpatil999@gmail.com>
2023-08-29 09:04:22 +0530

    max number updated

--- minMax.js
+++ minMax.js
@@ -1,5 +1,5 @@
 // https://practice.geeksforgeeks.org/problems/find-minimum-and-maximum-element-in-an-array4428/1
-const arr = [0,-1,-100,8, 12, 10, 6, 15];
+const arr = [0,-1,-100,8, 12, 10, 6, 15,1000];
 
 let min = Infinity;
 let max = 0;
